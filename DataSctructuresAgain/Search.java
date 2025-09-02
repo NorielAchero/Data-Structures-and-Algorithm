d91963f5a7d673ea49e7fa86b280eeeac6d8756f
Noriel Achero <acheronorielr@gmail.com>
2025-09-02 16:54:27 +0800

    haha

---
+++ DataSctructuresAgain/Search.java
@@ -0,0 +1,21 @@
+public class Search {
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 5, 7, 9, 11, 13, 15 };
+        int target = 7;
+
+        int result = binarySearch(arr, target);
+
+        System.out.println(result);
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+}
